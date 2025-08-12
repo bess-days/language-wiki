41626f68a10c7706bcabcd246ad46ace7d174c33
Sydney Greenspun <grimsby8610@gmail.com>
2025-08-12 14:05:17 -0700

    adjusted get_json function

--- tests/services_test.py
+++ tests/services_test.py
@@ -1,13 +1,26 @@
+from numpy.f2py.symbolic import Language
+
 from app.services import *
 REPO = Services()
 def test_language():
     l_test = REPO.search_by_lang("French")
     assert len(l_test) == 1
     assert type(l_test) == list
-    print(l_test)
+    for lang in l_test:
+        assert type(lang) == dict
+        for l in lang.values():
+            assert type(l) == dict
+            assert l["ISO-Code"] == "fr"
+            assert l["Family"] == "Indo-European"
+            assert l["Branch"] == "Romance"
+            assert l["Speakers"] == 312
+            assert l["Countries"] == 51
+            assert type(l["Scripts"]) == list
+            assert l["Scripts"] == ['Latin']
+
+    assert type(l_test) == list
 def test_family():
     f_test = REPO.search_by_family("Afro-Asiatic")
-    print(f_test)
     assert type(f_test) == list
     for lang in f_test:
         assert type(lang) == dict
